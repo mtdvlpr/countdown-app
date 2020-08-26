30c34b222ae27b2744d8131a87ddba3c4eb1bef1
Manoah <149895ja@gmail.com>
2020-08-26 17:30:29 +0200

    Updates

---
+++ precache-manifest.cd7c224f63a234f19b3616bcfc661ca5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c142f9f94ffdb086e3a2dc3f171a6be",
+    "url": "/countdown-app/index.html"
+  },
+  {
+    "revision": "38890374dd1525d36ec0",
+    "url": "/countdown-app/static/css/main.5026b96c.chunk.css"
+  },
+  {
+    "revision": "a2eb895157f43bbccc2d",
+    "url": "/countdown-app/static/js/2.5553b745.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/countdown-app/static/js/2.5553b745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38890374dd1525d36ec0",
+    "url": "/countdown-app/static/js/main.6f2260db.chunk.js"
+  },
+  {
+    "revision": "9f1fe4a54d4bd3ca1f32",
+    "url": "/countdown-app/static/js/runtime-main.fc5039be.js"
+  }
+]);
